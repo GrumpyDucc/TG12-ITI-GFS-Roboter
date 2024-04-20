3b43c8f64376513a402edbb70a667a5a48fd939e
GrumpyDucc <j.waldinger@online.de>
2024-04-20 09:36:34 +0200

    einstellbare geschwindigkeit

--- src/base.cpp
+++ src/base.cpp
@@ -3,36 +3,59 @@
 #include "mbed.h"
 #include "LCD.h"
 
+lcd mylcd;
+
 BufferedSerial hc05(PB_10, PB_11, 9600);
-PortOut motor(PortC, 0b11000011);
+
+PwmOut lv(PC_7);
+PwmOut lr(PC_6);
+PwmOut rr(PC_9);
+PwmOut rv(PC_8);
+
+float speed = 0.45;
+int new_speed = 0;
+bool readingFlag = false;
 
 int main()
 {
-    char daten[15];
+    char data[15];
 
     while (true)
     {
         if (hc05.readable())
         {
             thread_sleep_for(20);
-            hc05.read(daten, 1);
-            switch (daten[0])
+            hc05.read(data, 1);
+            switch (data[0])
             {
             case 'U':
-                motor = 0b10000010;
+                rv = speed;
+                lv = speed;
                 break; // vorwärts
             case 'R':
-                motor = 0b10000001;
+                rr = speed;
+                lv = speed;
                 break; // rechts
             case 'L':
-                motor = 0b01000010;
+                rv = speed;
+                lr = speed;
                 break; // links
             case 'D':
-                motor = 0b01000001;
+                rr = speed;
+                lr = speed;
                 break; // rückwärts
             case 'O':
-                motor = 0;
+                lv = 0;
+                lr = 0;
+                rv = 0;
+                rr = 0;
                 break; // aus
+            case 'A':
+                hc05.read(data, 2);             // 2-stellige Zahl nach A lesen
+                sscanf(data, "%d", &new_speed); // in zahl umwandeln
+                speed = float(new_speed) / 100; // zu korrektem float wandeln
+                hc05.read(data, 1);             // letztes A lesen
+                break;
             }
         }
     }
