e94cf01fd9efc8c44f06b864dd2770db4ba98a78
GrumpyDucc <j.waldinger@online.de>
2024-04-21 17:52:26 +0200

    bug fixes

---
+++ src/car_control.cpp
@@ -0,0 +1,149 @@
+#if 1
+
+#include "mbed.h"
+#include "LCD.h"
+
+lcd mylcd;
+
+BufferedSerial hc05(PB_10, PB_11, 9600);
+
+DigitalIn sensorRight(PA_1);
+DigitalIn sensorLeft(PA_2);
+DigitalIn manualDriveSwitch(PA_10);
+
+PwmOut lv(PC_7);
+PwmOut lr(PC_6);
+PwmOut rr(PC_9);
+PwmOut rv(PC_8);
+
+#define STANDARD_SPEED 0.50
+#define TURN_SPEED_FAST 1
+#define TURN_SPEED_SLOW 0.25
+#define TURNTIME 25
+
+float speed = STANDARD_SPEED; // minimum value for the car to move
+int new_speed = 0;
+bool followLine = false;
+
+void rightWheel(float speed)
+{
+    printf("rw: %d\n", int(speed * 100));
+    if (speed > 0) // vorwärts
+    {
+        rv = speed;
+        rr = 0;
+    }
+    else // rückwärts
+    {
+        rv = 0;
+        rr = speed * -1;
+    }
+}
+
+void leftWheel(float speed)
+{
+    printf("lw: %d\n", int(speed * 100));
+    if (speed > 0) // vorwärts
+    {
+        lv = speed;
+        lr = 0;
+    }
+    else // rückwärts
+    {
+        lv = 0;
+        lr = speed * -1;
+    }
+}
+
+void stop()
+{
+    lv = 0;
+    lr = 0;
+    rr = 0;
+    rv = 0;
+}
+
+void switchDriveMode()
+{
+    followLine = !followLine; // Fahrmodus umschalten
+    stop();                   // Auto anhalten
+}
+
+void init()
+{
+    mylcd.clear();
+    sensorLeft.mode(PullDown);
+    sensorRight.mode(PullDown);
+    manualDriveSwitch.mode(PullDown);
+}
+
+int main()
+{
+    init();
+    char data[15];
+
+    while (true)
+    {
+        if (manualDriveSwitch) // manueller Fahrmoduswechsel
+        {
+            switchDriveMode();
+            thread_sleep_for(250);
+        }
+        if (hc05.readable())
+        {
+            thread_sleep_for(20);
+            hc05.read(data, 1);
+            switch (data[0])
+            {
+            case 'F':
+                switchDriveMode();
+                break;
+            case 'U': // vorwärts
+                rightWheel(speed);
+                leftWheel(speed);
+                break;
+            case 'R': // rechts
+                rightWheel(speed * -1);
+                leftWheel(speed);
+                break;
+            case 'L': // links
+                rightWheel(speed);
+                leftWheel(speed * -1);
+                break;
+            case 'D': // rückwärts
+                rightWheel(speed * -1);
+                leftWheel(speed * -1);
+                break;
+            case 'O': // aus
+                stop();
+                followLine = false;
+                break;
+            case 'A':                           // Geschwindigkeitsänderung feststellen
+                hc05.read(data, 3);             // 2-stellige Zahl und das end "A" lesen
+                sscanf(data, "%d", &new_speed); // in zahl umwandeln
+                speed = float(new_speed) / 100; // zu korrektem float wandeln
+                break;
+            }
+        }
+        if (followLine)
+        {
+            if (sensorRight.read() == 1) // rechter Sensor über schwarz -> nach links lenken
+            {
+                rightWheel(TURN_SPEED_SLOW);
+                leftWheel(TURN_SPEED_FAST);
+            }
+            else if (sensorLeft.read() == 1) // linker Sensor über schwarz -> nach rechts lenken
+            {
+                rightWheel(TURN_SPEED_FAST);
+                leftWheel(TURN_SPEED_SLOW);
+            }
+            else // beide Sensoren über weiß oder beide über schwarz-> geradeaus fahren
+            {
+                rightWheel(STANDARD_SPEED);
+                leftWheel(STANDARD_SPEED);
+            }
+        }
+    }
+}
+
+#endif
